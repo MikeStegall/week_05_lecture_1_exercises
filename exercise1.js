1e12a7d01b25abdcd2ee6d008500fa16192ad362
MikeStegall <mestegall5@gmail.com>
2017-03-25 09:23:42 -0500

    finished exerciese1.ja

---
+++ exercise1.js
@@ -0,0 +1,16 @@
+// Each of the following blocks of JavaScript contains errors. Take a look at each and do the following:
+// Identify which line the error occurs on.
+// Explain the cause of the error
+// Fix the error, so the code produces the desired response.
+
+// var firstName = "Julia";
+// var lastName = "Roberts";
+// var fullName = firstName + lastname;
+//
+// console.log(fullName); // Julia Roberts
+
+var firstName = 'Julia'
+var lastName = 'Roberts'
+var fullName = firstName + lastName
+
+console.log(fullName) // Julia Roberts
